5c2a160903e60eb7cfd1beed194c5509a851ceb5
MaryAnne45 <mary.jereh@gmail.com>
2018-10-07 18:22:50 +0300

    added a logo

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="app">
-  <div class="container">
-    <router-view/>
-  </div>
+    <router-view />
   </div>
 </template>
 
@@ -11,7 +9,3 @@ export default {
   name: 'App'
 }
 </script>
-
-<style>
-
-</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,18 +4,18 @@
   <div class="dashboard">
     <nav class="navbar navbar-dark bg-dark">
       <a class="navbar-brand" href="#">
-        <img src="https://a.storyblok.com/f/39898/1024x1024/dea4e1b62d/vue-js_logo-svg.png" width="40" height="40">
+        <img src="../assets/Octocat.png" width="40" height="40">
       </a>
       <div>
-        <img :src="$auth.user.picture" width="30" height="30">
-        <span class="text-muted font-weight-light px-2">{{$auth.user.name}}</span>
+        <img :src="$auth.user.picture" width="40" height="40">
+        <span class="text-muted font-weight-light px-2">{{$auth.user.name}} </span>
         <button type="button" class="btn btn-outline-secondary btn-sm" @click="$auth.logout()">Logout</button>
       </div>
     </nav>
 
     <div class="jumbotron">
       <div class="container">
-        <h1 class="display-4">Hello, {{$auth.user.name}}!</h1>
+        <h1 class="display-4">Hello, {{$auth.user.name}}!{{$auth.user.email}}</h1>
         <p class="lead">We hope you liked this tutorial and can now start building new astounding projects from this start point. If you're interested in what we're doing besides tech tutorials check out <a href="https://www.storyblok.com">@storyblok</a>.</p>
         <hr class="my-4">
         <p>TBH, I'm sure this project of yours would look great with a landing page filled with content composed in <a href="https://www.storyblok.com">Storyblok</a> 🎉</p>
